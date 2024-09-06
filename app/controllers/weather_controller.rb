8b80c399220847bea732a1b2cb967e7a74a26ce1
amandac3600 <amandac3600@gmail.com>
2024-09-06 16:10:31 -0700

    add 5 day forecast

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -18,10 +18,10 @@ class WeatherController < ApplicationController
 			end
 			@weather = @weather_data["weather"][0]
       @description = @weather["description"].capitalize
-      @temperature_in_f = "#{((@weather_data["main"]["temp"] * 9/5) + 32).round(0)}°F"
-      @temperature_in_c = "#{@weather_data["main"]["temp"]}°C"
-      @feels_like_in_f = "#{((@weather_data["main"]["feels_like"] * 9/5) + 32).round(0)}°F"
-      @feels_like_in_c = "#{@weather_data["main"]["feels_like"]}°C"
+      @temperature_in_f = "#{@weather_data["main"]["temp"].round(0)}°F"
+      @temperature_in_c = "#{((@weather_data["main"]["temp"] - 32) * 9/5).round(0)}°C"
+      @feels_like_in_f = "#{@weather_data["main"]["feels_like"].round(0)}°F"
+      @feels_like_in_c = "#{((@weather_data["main"]["feels_like"] - 32) * 9/5).round(0)}°C"
 			@location = @weather_data["name"].blank? ? "Unknown Location" : "#{@weather_data["name"]}, #{@weather_data["sys"]["country"]}"
 
       # TODO: create helper method for timezone
@@ -32,10 +32,25 @@ class WeatherController < ApplicationController
       timezone_string = ActiveSupport::TimeZone.all.find { |zone| zone.formatted_offset == formatted_offset }
       @sunrise = Time.at(@weather_data["sys"]["sunrise"]).utc.in_time_zone(timezone_string).strftime("%I:%M %p %Z")
       @sunset = Time.at(@weather_data["sys"]["sunset"]).utc.in_time_zone(timezone_string).strftime("%I:%M %p %Z")
-      @max_in_f = "#{((@weather_data["main"]["temp_max"] * 9/5) + 32).round(0)}°F"
-      @max_in_c = "#{@weather_data["main"]["temp_max"]}°C"
-      @min_in_f = "#{((@weather_data["main"]["temp_min"] * 9/5) + 32).round(0)}°F"
-      @min_in_c = "#{@weather_data["main"]["temp_min"]}°C"
+      @max_in_f = "#{@weather_data["main"]["temp_max"].round(0)}°F"
+      @max_in_c = "#{((@weather_data["main"]["temp_max"] - 32) * 9/5).round(0)}°C"
+      @min_in_f = "#{@weather_data["main"]["temp_min"].round(0)}°F"
+      @min_in_c = "#{((@weather_data["main"]["temp_min"] - 32) * 9/5).round(0)}°C"
+
+      @five_day_forecast = @forecast_data["list"].group_by { |entry| entry["dt_txt"].split(" ").first }
+
+      # @five_day_forecast.delete(@forecast_data["list"][0]["dt_txt"].split(" ").first)
+      @five_day_forecast.each_with_index do |(date, entries), i|
+        day_data = { 
+          date: Date.parse(date).strftime("%a %-m/%-d"),
+          temp_max: "#{entries.map { |entry| entry['main']['temp_max'] }.max.round(0)}°F",
+          temp_min: "#{entries.map { |entry| entry['main']['temp_min'] }.min.round(0)}°F",
+          description: (entries.map { |entry| entry["weather"].map { |weather| weather["description"]}}.group_by { |ele| ele }.max_by { |ele, occurrences| occurrences.size }.first).first,
+          # This will return the icon for the most common weather description for the day
+          icon: (entries.map { |entry| entry["weather"].map { |weather| weather["icon"]}}.group_by { |ele| ele }.max_by { |ele, occurrences| occurrences.size }.first).first,
+        }
+        instance_variable_set("@day_#{i + 1}", day_data)
+      end
     else
       redirect_to map_path, alert: "Invalid location."
     end
@@ -46,7 +61,7 @@ class WeatherController < ApplicationController
   def fetch_weather(lat, lng)
     # TODO: make this secret later
     api_key = "3bc195e34264aed920aed88f03df7554"
-    uri = URI("https://api.openweathermap.org/data/2.5/weather?lat=#{lat}&lon=#{lng}&appid=#{api_key}&units=metric&lang=en")
+    uri = URI("https://api.openweathermap.org/data/2.5/weather?lat=#{lat}&lon=#{lng}&appid=#{api_key}&units=imperial&lang=en")
     response = Net::HTTP.get(uri)
     JSON.parse(response)
   end
@@ -54,7 +69,8 @@ class WeatherController < ApplicationController
   def fetch_forecast(lat, lng)
     # TODO: make this secret later
     api_key = "3bc195e34264aed920aed88f03df7554"
-    uri = URI("https://api.openweathermap.org/data/2.5/forecast?lat=#{lat}&lon=#{lng}&appid=#{api_key}&units=metric&lang=en")
+    # uri = URI("https://api.openweathermap.org/data/2.5/onecall?lat=#{lat}&lon=#{lng}&appid=#{api_key}")
+    uri = URI("https://api.openweathermap.org/data/2.5/forecast?lat=#{lat}&lon=#{lng}&appid=#{api_key}&units=imperial&lang=en")
     response = Net::HTTP.get(uri)
     JSON.parse(response)
   end
